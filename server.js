fa0b6946692d47ccfda2b5b1d1bccb255e797f23
Mario <mariopereztoranzo1@gmail.com>
2025-04-21 21:12:10 +0200

    Add gitignore and update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use('/api/items', itemsRouter);
 app.use('/api/pedidos', pedidosRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
