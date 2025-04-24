19517509cfed78b226f6e000811cdad3eb592436
Mario <mariopereztoranzo1@gmail.com>
2025-04-24 10:54:49 +0200

    provedor

--- models/item.js
+++ models/item.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
   nombre: String,
   cantidad: Number,
-  descripcion: String
+  descripcion: String,
+  proveedor: String
 });
 
 module.exports = mongoose.model('Item', itemSchema);
